fcac7134f975ef624e1560bd18f6f2cee4b7eada
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-17 19:52:30 +0300

    2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SYMBOL_X = 'X';
 const SYMBOL_O = 'O';
 
 function App() {
-   const cells = [null, null, null, SYMBOL_O, SYMBOL_X, SYMBOL_O, null, null, null];
+   const cells = [SYMBOL_O, null, null, SYMBOL_O, SYMBOL_X, null, null, null, null];
    const currentStep = SYMBOL_X;
 
    const getSymbolClassName = (symbol) => {
@@ -11,24 +11,19 @@ function App() {
       return '';
    };
 
+   const renderSymbol = (symbol) => (
+      <span className={`symbol ${getSymbolClassName(symbol)}`}>{symbol}</span>
+   );
+
    return (
       <div className="game">
-         <div className="game-info">
-            Ход:{' '}
-            <span className={`symbol ${getSymbolClassName(currentStep)}`}>
-               {currentStep}
-            </span>
-         </div>
+         <div className="game-info">Ход: {renderSymbol(currentStep)}</div>
 
          <div className="game-field">
-            {cells.map((symbol) => {
+            {cells.map((symbol, index) => {
                return (
-                  <button className="cell">
-                     {symbol ? (
-                        <span className={`symbol ${getSymbolClassName(symbol)}`}>
-                           {symbol}
-                        </span>
-                     ) : null}
+                  <button key={index} className="cell">
+                     {symbol ? renderSymbol(symbol) : null}
                   </button>
                );
             })}
