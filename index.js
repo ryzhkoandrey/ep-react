b86b68cbffd7c0d78175a6bb0b5a6894aeb5b9dc
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-17 19:31:10 +0300

    2

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
+const SYMBOL_X = 'X';
+const SYMBOL_O = 'O';
+
 function App() {
-   return <div>Hello world</div>;
+   const cells = [null, null, null, SYMBOL_O, SYMBOL_X, SYMBOL_O, null, null, null];
+   const currentStep = SYMBOL_X;
+
+   const getSymbolClassName = (symbol) => {
+      if (symbol === SYMBOL_O) return 'symbol--o';
+      if (symbol === SYMBOL_X) return 'symbol--x';
+      return '';
+   };
+
+   return (
+      <div className="game">
+         <div className="game-info">
+            Ход:{' '}
+            <span className={`symbol ${getSymbolClassName(currentStep)}`}>
+               {currentStep}
+            </span>
+         </div>
+
+         <div className="game-field">
+            {cells.map((symbol) => {
+               return (
+                  <button className="cell">
+                     {symbol ? (
+                        <span className={`symbol ${getSymbolClassName(symbol)}`}>
+                           {symbol}
+                        </span>
+                     ) : null}
+                  </button>
+               );
+            })}
+         </div>
+      </div>
+   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
