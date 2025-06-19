d4aec68157ee6b04a6559b735f01f73b5eaf9575
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-19 22:15:08 +0300

    12

--- 02 JSX/index.js
+++ 02 JSX/index.js
@@ -1,5 +1,17 @@
 function App() {
-   return <div>Hello world</div>;
+   const [counter, setCounter] = React.useState(0);
+
+   const increment = (count = 1) => {
+      setCounter(counter + count);
+   };
+
+   return (
+      <div>
+         <div>Счетчик: {counter}</div>
+         <button onClick={() => increment()}>+1</button>
+         <button onClick={() => increment(3)}>+3</button>
+      </div>
+   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
