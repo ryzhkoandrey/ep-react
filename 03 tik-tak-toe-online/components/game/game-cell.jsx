7b2e47641ae012438a0fb5f066fb60a03650e064
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-21 01:06:18 +0300

    17

--- 03 tik-tak-toe-online/components/game/game-cell.jsx
+++ 03 tik-tak-toe-online/components/game/game-cell.jsx
@@ -1,11 +1,13 @@
 import { GameSymbol } from './game-symbol';
+import { clsx } from 'clsx';
 
 export function GameCell({ isWinner, onClick, symbol }) {
    return (
       <button
-         className={`border border-gray-400 -mt-px -ml-px flex items-center justify-center bg-transparent ${
-            isWinner ? 'bg-green-100' : ''
-         }`}
+         className={clsx(
+            'border border-gray-400 -mt-px -ml-px flex items-center justify-center bg-transparent',
+            isWinner && 'bg-green-100'
+         )}
          onClick={onClick}
       >
          {symbol ? <GameSymbol symbol={symbol} /> : null}
