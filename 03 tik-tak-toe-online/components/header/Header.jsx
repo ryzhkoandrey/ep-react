bfcec545946a4664814c17141ab4bc75e04b3a5c
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-21 21:30:02 +0300

    20

--- 03 tik-tak-toe-online/components/header/Header.jsx
+++ 03 tik-tak-toe-online/components/header/Header.jsx
@@ -4,7 +4,9 @@ import logoSrc from './logo.svg';
 export function Header() {
    return (
       <header className="flex items-center h-24 px-8 bg-white shadow-lg">
-         <Image src={logoSrc} />
+         <Image src={logoSrc} alt="logo" />
+
+         <div className="w-px h-8 mx-6 bg-slate-200"></div>
       </header>
    );
 }
