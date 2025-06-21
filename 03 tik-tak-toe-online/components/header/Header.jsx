ab8642c6d8741938fa642751277ffb34199ce226
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-21 22:04:23 +0300

    20

--- 03 tik-tak-toe-online/components/header/Header.jsx
+++ 03 tik-tak-toe-online/components/header/Header.jsx
@@ -6,7 +6,11 @@ export function Header() {
       <header className="flex items-center h-24 px-8 bg-white shadow-lg">
          <Image src={logoSrc} alt="logo" />
 
-         <div className="w-px h-8 mx-6 bg-slate-200"></div>
+         <div className="w-px h-8 bg-slate-200 mx-6" />
+
+         <button className="w-44 bg-teal-600 text-white rounded-lg px-5 py-2 text-2xl">
+            Играть
+         </button>
       </header>
    );
 }
--- 03 tik-tak-toe-online/pages/_app.jsx
+++ 03 tik-tak-toe-online/pages/_app.jsx
@@ -1,6 +1,12 @@
-import "../styles/global.css";
+import '../styles/global.css';
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin', 'cyrillic'] });
 
 export default function App({ Component, pageProps }) {
-  console.log("app");
-  return <Component {...pageProps} />;
+   console.log('app');
+   return (
+      <div className={inter.className}>
+         <Component {...pageProps} />
+      </div>
+   );
 }
