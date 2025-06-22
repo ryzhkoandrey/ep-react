2069a80699eaa03f387f6dfcd0ec8a8ba043d1fc
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-22 21:58:32 +0300

    21

--- 03 tik-tak-toe-online/components/game/game-info.jsx
+++ 03 tik-tak-toe-online/components/game/game-info.jsx
@@ -1,5 +1,18 @@
 import clsx from 'clsx';
+import { Profile } from '../profile';
+import { CrossIcon } from './icons/cross-icon';
 
 export function GameInfo({ className }) {
-   return <div className={clsx(className, '')}>GameInfo</div>;
+   return (
+      <div className={clsx(className, 'bg-white rounded-2xl shadow-md px-8 py-4')}>
+         <div>
+            <div className="relative">
+               <Profile />
+               <div className="w-5 h-5 flex items-center justify-center rounded-full bg-white shadow absolute -left-1 -top-1">
+                  <CrossIcon />
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
