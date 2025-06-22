c592b3e5272c3d1ec3208dfefc08c816ee323f20
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-22 16:05:57 +0300

    21

--- 03 tik-tak-toe-online/components/game/game-title.jsx
+++ 03 tik-tak-toe-online/components/game/game-title.jsx
@@ -3,11 +3,16 @@ import { ArrowLeftIcon } from './icons/arrow-left-icon';
 
 export function GameTitle() {
    return (
-      <div className="pl-2">
-         <Link className="flex items-center gap-2 text-teal-600" href="#">
+      <div className="pl-2 flex flex-col items-start">
+         <Link
+            className="flex items-center gap-2 text-xs text-teal-600 leading-tight -mb-0.5"
+            href="#"
+         >
             <ArrowLeftIcon />
             На главную
          </Link>
+
+         <h1 className="text-4xl leading-tight">Крестики нолики</h1>
       </div>
    );
 }
