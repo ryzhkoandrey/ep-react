bf4560b8bce4998444d6145b42d4e48e26f014bb
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-23 19:15:53 +0300

    22

--- 03 tik-tak-toe-online/components/game/game-field.jsx
+++ 03 tik-tak-toe-online/components/game/game-field.jsx
@@ -3,11 +3,14 @@ import { ZeroIcon } from './icons/zero-icon';
 import { CrossIcon } from './icons/cross-icon';
 import { UiButton } from '../uikit/ui-button';
 
+const cells = new Array(19 * 19).fill(null);
+
 export function GameField({ className }) {
    return (
       <div
          className={clsx(className, 'bg-white rounded-2xl shadow-md px-8 pt-5 pb-7')}
       >
+         {/* header */}
          <div className="flex items-center gap-3">
             {/* info */}
             <div className="mr-auto">
@@ -28,6 +31,16 @@ export function GameField({ className }) {
             <UiButton size="md" variant="outline">
                Сдаться
             </UiButton>
+         </div>
+
+         {/* grid */}
+         <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pl-px pt-px mt-4">
+            {cells.map((_, i) => (
+               <button
+                  key={i}
+                  className="border border-slate-200 -ml-px -mt-px"
+               ></button>
+            ))}
          </div>
       </div>
    );
