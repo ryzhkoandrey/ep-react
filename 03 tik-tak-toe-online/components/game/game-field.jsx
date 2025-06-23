28425eca35fdb57d4354abced6b2f277965b2707
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-23 21:50:58 +0300

    23

--- 03 tik-tak-toe-online/components/game/game-field.jsx
+++ 03 tik-tak-toe-online/components/game/game-field.jsx
@@ -4,52 +4,34 @@ import { CrossIcon } from './icons/cross-icon';
 import { UiButton } from '../uikit/ui-button';
 
 const cells = new Array(19 * 19).fill(null);
+const actions = (
+   <>
+      <UiButton size="md" variant="primary">
+         Ничья
+      </UiButton>
+      <UiButton size="md" variant="outline">
+         Сдаться
+      </UiButton>
+   </>
+);
 
 export function GameField({ className }) {
    return (
-      <div
-         className={clsx(className, 'bg-white rounded-2xl shadow-md px-8 pt-5 pb-7')}
-      >
-         {/* header */}
-         <div className="flex items-center gap-3">
-            {/* info */}
-            <div className="mr-auto">
-               <div className="flex items-center gap-1 text-xl leading-tight font-semibold">
-                  Ход: <ZeroIcon className="w-5 h-5" />
-               </div>
-               <div className="flex items-center gap-1 text-xs leading-tight text-slate-400">
-                  Следующий: <CrossIcon />
-               </div>
-            </div>
+      <GameFieldLayout className={className}>
+         <GameMoveInfo actions={actions}></GameMoveInfo>
 
-            {/* btns */}
-            <UiButton size="md" variant="primary">
-               Ничья
-            </UiButton>
-            <UiButton size="md" variant="outline">
-               Сдаться
-            </UiButton>
-         </div>
-
-         {/* grid */}
-         <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pl-px pt-px mt-4">
-            {cells.map((_, i) => (
-               <button
-                  key={i}
-                  className="border border-slate-200 -ml-px -mt-px flex items-center justify-center"
-               ></button>
+         <GameGrid>
+            {cells.map((_, index) => (
+               <GameCell key={index}></GameCell>
             ))}
-         </div>
-      </div>
+         </GameGrid>
+      </GameFieldLayout>
    );
 }
 
 function GameCell({ children }) {
    return (
-      <button
-         key={i}
-         className="border border-slate-200 -ml-px -mt-px flex items-center justify-center"
-      >
+      <button className="border border-slate-200 -ml-px -mt-px flex items-center justify-center">
          {children}
       </button>
    );
