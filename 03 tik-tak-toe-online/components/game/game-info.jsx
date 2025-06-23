53eaa0f982f5f625d0468717cc11fd68b4b20493
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-23 09:58:08 +0300

    21

--- 03 tik-tak-toe-online/components/game/game-info.jsx
+++ 03 tik-tak-toe-online/components/game/game-info.jsx
@@ -5,13 +5,17 @@ import { CrossIcon } from './icons/cross-icon';
 export function GameInfo({ className }) {
    return (
       <div className={clsx(className, 'bg-white rounded-2xl shadow-md px-8 py-4')}>
-         <div>
+         <div className="flex items-center gap-3">
+            {/* profile */}
             <div className="relative">
-               <Profile />
+               <Profile className="w-[180px]" />
                <div className="w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1 flex items-center justify-center">
                   <CrossIcon />
                </div>
             </div>
+
+            {/* divider */}
+            <div className="w-px h-6 bg-slate-200" />
          </div>
       </div>
    );
