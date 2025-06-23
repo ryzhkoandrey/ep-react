89ba40a325f36a4869dd0abca8189938aa60cbc8
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-23 10:26:49 +0300

    21

--- 03 tik-tak-toe-online/components/game/game-info.jsx
+++ 03 tik-tak-toe-online/components/game/game-info.jsx
@@ -1,21 +1,42 @@
 import clsx from 'clsx';
 import { Profile } from '../profile';
 import { CrossIcon } from './icons/cross-icon';
+import { ZeroIcon } from './icons/zero-icon';
 
 export function GameInfo({ className }) {
    return (
-      <div className={clsx(className, 'bg-white rounded-2xl shadow-md px-8 py-4')}>
+      <div
+         className={clsx(
+            className,
+            'bg-white rounded-2xl shadow-md px-8 py-4 flex justify-between'
+         )}
+      >
+         {/* user */}
          <div className="flex items-center gap-3">
-            {/* profile */}
             <div className="relative">
-               <Profile className="w-[180px]" />
+               <Profile className="w-44" />
                <div className="w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1 flex items-center justify-center">
                   <CrossIcon />
                </div>
             </div>
 
-            {/* divider */}
             <div className="w-px h-6 bg-slate-200" />
+
+            <div className="text-slate-900 text-lg font-semibold">01:08</div>
+         </div>
+
+         {/* user */}
+         <div className="flex items-center gap-3">
+            <div className="text-orange-600 text-lg font-semibold">00:08</div>
+
+            <div className="w-px h-6 bg-slate-200" />
+
+            <div className="relative">
+               <Profile className="w-44" />
+               <div className="w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1 flex items-center justify-center">
+                  <ZeroIcon />
+               </div>
+            </div>
          </div>
       </div>
    );
