30c0bae8f0e891bcc8b9f8e66ec9500d4297c78e
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-26 21:10:42 +0300

    27

--- 03 tik-tak-toe-online/components/game/use-game-state.js
+++ 03 tik-tak-toe-online/components/game/use-game-state.js
@@ -7,8 +7,19 @@ function getNextMove(currentMove, playersCount) {
    return slicedMoveOrder[nextMoveIndex] ?? slicedMoveOrder[0];
 }
 
-function computeWinner(cells, sequenceSize = 5, fieldSize = 19) {
+function computeWinner(cells, sequenceSize = 3, fieldSize = 19) {
    const gap = Math.floor(sequenceSize / 2);
+
+   function compareElements(indexes) {
+      let result = true;
+
+      for (let i = 1; i < indexes.length; i++) {
+         result &&= !!cells[indexes[i]];
+         result &&= cells[indexes[i]] === cells[indexes[i - 1]];
+      }
+
+      return result;
+   }
 
    function getSequenceIndexes(i) {
       const res = [
@@ -30,9 +41,15 @@ function computeWinner(cells, sequenceSize = 5, fieldSize = 19) {
 
    for (let i = 0; i < cells.length; i++) {
       if (cells[i]) {
-         console.log(getSequenceIndexes(i));
+         const indexRows = getSequenceIndexes(i);
+         const winnerIndexes = indexRows.find((row) => compareElements(row));
+         if (winnerIndexes) {
+            return winnerIndexes;
+         }
       }
    }
+
+   return undefined;
 }
 
 export function useGameState(playersCount) {
