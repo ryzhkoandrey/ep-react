976dc5548cc2a0a3eeb68a101ee685d0993978dc
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-07 12:13:39 +0300

    31

--- 03 tik-tak-toe-online/components/game-new/game.jsx
+++ 03 tik-tak-toe-online/components/game-new/game.jsx
@@ -1,5 +1,16 @@
 import { GameLayouts } from './ui/game-layout';
+import { BackLink } from './ui/back-link';
+import { GameInfo } from './ui/game-info';
+import { GameTitle } from './ui/game-title';
 
 export function Game() {
-   return <GameLayouts />;
+   return (
+      <GameLayouts
+         backLink={<BackLink />}
+         title={<GameTitle />}
+         gameInfo={
+            <GameInfo isRatingGame playersCount={4} timeMode={'1 мин. на ход'} />
+         }
+      />
+   );
 }
--- 03 tik-tak-toe-online/components/game-new/ui/game-info.jsx
+++ 03 tik-tak-toe-online/components/game-new/ui/game-info.jsx
@@ -2,17 +2,17 @@ import { StarIcon } from './icons/star-icon';
 import { UserIcon } from './icons/user-icon';
 import { HistoryIcon } from './icons/history-icon';
 
-export function GameInfo() {
+export function GameInfo({ playersCount, isRatingGame, timeMode }) {
    return (
       <div className="flex items-center gap-3 text-xs text-slate-400">
-         <StarIcon />
+         {isRatingGame && <StarIcon />}
 
          <div className="flex items-center gap-1">
             <UserIcon /> {playersCount}
          </div>
 
          <div className="flex items-center gap-1">
-            <HistoryIcon /> 1 мин на ход
+            <HistoryIcon /> {timeMode}
          </div>
       </div>
    );
